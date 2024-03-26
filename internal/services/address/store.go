ba83f0373a5d78f584600acd95c7d8c6230021f8
Claudiney Veloso <claudineyveloso@gmail.com>
2024-03-26 13:09:24 -0300

    created store to services address

--- internal/services/address/store.go
+++ internal/services/address/store.go
@@ -1,6 +1,16 @@
 package address
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/claudineyveloso/bookernet.git/internal/db"
+	"github.com/claudineyveloso/bookernet.git/internal/types"
+	"github.com/claudineyveloso/bookernet.git/internal/utils"
+	"github.com/google/uuid"
+)
 
 type Store struct {
 	db *sql.DB
@@ -8,4 +18,33 @@ type Store struct {
 
 func NewStore(db *sql.DB) *Store {
 	return &Store{db: db}
+}
+
+func (s *Store) CreateAddress(address types.CreateAddressPayload) error {
+	queries := db.New(s.db)
+	ctx := context.Background()
+
+	address.ID = uuid.New()
+	now := time.Now()
+	address.CreatedAt = now
+	address.UpdatedAt = now
+
+	createAddressParams := db.CreateAddressParams{
+		ID:              address.ID,
+		PublicPlace:     utils.CreateNullString(address.PublicPlace),
+		Complement:      utils.CreateNullString(address.Complement),
+		Neighborhood:    utils.CreateNullString(address.Neighborhood),
+		City:            utils.CreateNullString(address.City),
+		State:           utils.CreateNullString(address.State),
+		ZipCode:         utils.CreateNullString(address.ZipCode),
+		AddressableID:   address.AddressableID,
+		AddressableType: address.AddressableType,
+		CreatedAt:       address.CreatedAt,
+		UpdatedAt:       address.UpdatedAt,
+	}
+	if err := queries.CreateAddress(ctx, createAddressParams); err != nil {
+		fmt.Println("Erro ao criar o endereço:", err)
+		return err
+	}
+	return nil
 }
