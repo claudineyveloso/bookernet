22720110b0512a1f261d0e07cc95e717e25e6ecf
Claudiney Veloso <claudineyveloso@gmail.com>
2024-03-26 14:37:30 -0300

    create service person

--- internal/services/person/store.go
+++ internal/services/person/store.go
@@ -1,6 +1,16 @@
 package person
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/claudineyveloso/bookernet.git/internal/db"
+	"github.com/claudineyveloso/bookernet.git/internal/types"
+	"github.com/claudineyveloso/bookernet.git/internal/utils"
+	"github.com/google/uuid"
+)
 
 type Store struct {
 	db *sql.DB
@@ -8,4 +18,32 @@ type Store struct {
 
 func NewStore(db *sql.DB) *Store {
 	return &Store{db: db}
+}
+
+func (s *Store) CreatePerson(person types.CreatePersonPayload) error {
+	queries := db.New(s.db)
+	ctx := context.Background()
+
+	person.ID = uuid.New()
+	now := time.Now()
+	person.CreatedAt = now
+	person.UpdatedAt = now
+
+	createPersonParams := db.CreatePersonParams{
+		ID:             person.ID,
+		FirstName:      person.FirstName,
+		LastName:       person.LastName,
+		Email:          person.Email,
+		Phone:          utils.CreateNullString(person.Phone),
+		CellPhone:      person.CellPhone,
+		PersonableID:   person.PersonableID,
+		PersonableType: person.PersonableType,
+		CreatedAt:      person.CreatedAt,
+		UpdatedAt:      person.UpdatedAt,
+	}
+	if err := queries.CreatePerson(ctx, createPersonParams); err != nil {
+		fmt.Println("Erro ao criar o endereço:", err)
+		return err
+	}
+	return nil
 }
